16e100b10edbdf7bae41fe6267f2d9efdd6fa066
kalpadiptyaroy <40118928+kalpadiptyaroy@users.noreply.github.com>
2022-09-09 21:07:08 +0530

    Update QuickSort.c
    Added Comments to easily understand the algorithm.

--- QuickSort.c
+++ QuickSort.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include "Stack.h"
 
-void display(int *a, int n)
+void display(int *a, int n)			// Function to display array.
 {
 	int i;
 	for(i = 0; i < n; i++)
@@ -10,15 +10,15 @@ void display(int *a, int n)
 	printf("\n");
 }
 
-void finput(int *a, int n)
+void finput(int *a, int n)			// Function to take inputs from the file Number.txt and store them in array.
 {
 	int i;
-	FILE *fp = fopen("Number.txt", "r");
+	FILE *fp = fopen("Number.txt", "r");	// File pointer
 	for(i = 0; i < n; i++)
 		fscanf(fp, "%d", a + i);	
 }
 
-void input(int *a, int n)
+void input(int *a, int n)			// Function for console input and store them in array.
 {
 	int i;
 	for(i = 0; i < n; i++)
@@ -28,37 +28,37 @@ void input(int *a, int n)
 	}
 }
 
-void quicksort(int *a, int n)
+void quicksort(int *a, int n)			// QuickSort implementation using interative technique.
 {
-	Stack stack[10];
+	Stack stack[10];			
 	int small, big, top = -1, t, begin, end;
 	
 	begin = 0, end = n - 1;
 
-	while(begin < end || pop(stack, &top, &begin, &end))
+	while(begin < end || pop(stack, &top, &begin, &end))	// Loop terminates only when begin index has crossed end index AND stack is empty.
 	{
 		
-		printf("\nBegin = %d End = %d Stack Top = %d \n", begin, end, top);
-		display(a, n);
-		small = begin + 1;
-		while(small <= end && a[small] < a[begin])
-			small++;
-		big = small--;
+		printf("\nBegin = %d End = %d Stack Top = %d \n", begin, end, top); // Debugging
+		display(a, n);							    // Display the state of array at each iteration.
+		small = begin + 1;						    // The first element pointed by begin is taken as pivot.
+		while(small <= end && a[small] < a[begin])			    // Elements smaller than pivot starts from begin + 1 onwards.
+			small++;						    // We keep on increasing small until we get a bigger element than pivot.
+		big = small--;							    // Loop stops if we get bigger element than pivot and we set big to that index and correct small to the previous index.
 		
-		while(big <= end)
+		while(big <= end)						    // We iterate from newly pointed big index to the end.
 		{
 			if(a[big] < a[begin])
-			{
-				t = a[big];
+			{							    // if we get a smaller number than pivot then we swap it with the 1st big element.
+				t = a[big];					    // After swapping we increase small var. by 1 and big var. by 1
 				a[big] = a[small + 1];
 				a[small++ + 1] = t;
 			}
 			big++;
 		}
-		t = a[begin];	a[begin] = a[small];	a[small] = t;
+		t = a[begin];	a[begin] = a[small];	a[small] = t;		    // we swap the pivot with the pivot with the last small element.
 		
 		if(small + 1 < end)
-			push(stack, &top, 10, small + 1, end);
+			push(stack, &top, 10, small + 1, end);			    // if a subarray of bigger elements than pivot exists then push that subarray limits in stack.
 		end = small - 1;
 	}
 }
